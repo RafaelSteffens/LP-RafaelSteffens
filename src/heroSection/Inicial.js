9f84feaf99eaf554b8e84f22063adbde2de82af4
Rafael Steffens <rafaelfsteffens@gmail.com>
2025-04-24 15:57:57 -0300

    Foi corriido alguns estilos e animações

--- src/heroSection/Inicial.js
+++ src/heroSection/Inicial.js
@@ -2,17 +2,31 @@ import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import styles from './Inicial.module.css';
 
+
+
+
 const HeroSection = () => {
   const fullText = 'Olá, eu sou o Rafael Steffens ;) ';
   const [displayedText, setDisplayedText] = useState('');
+  const [textClass, setTextClass] = useState('');
+
+  const [animateImage, setAnimateImage] = useState(false);
 
   useEffect(() => {
     let i = 0;
     const interval = setInterval(() => {
       setDisplayedText(fullText.slice(0, i + 1));
       i++;
-      if (i === fullText.length) clearInterval(interval);
+      if (i === fullText.length) {
+        clearInterval(interval);
+        setTimeout(() => setTextClass(styles.glow), 300); // adiciona glow depois
+      }
     }, 50);
+    setTimeout(() => {
+      setTextClass(styles.glow);
+      setAnimateImage(true); // <- ativa animação da imagem
+    }, 300);
+    
     return () => clearInterval(interval);
   }, []);
 
@@ -24,10 +38,10 @@ const HeroSection = () => {
         transition={{ duration: 1 }}
         className={styles.textContainer}
       >
-        <h3 className={styles.title}>{displayedText}</h3>
-        <br></br>
+        <h3 className={`${styles.title} ${textClass}`}>{displayedText}</h3>
+        <br />
         <p>Sou dev fullstack, como posso te ajudar?</p>
-        <br></br>
+        <br />
         <div className={styles.actions}>
           <motion.a
             whileHover={{ scale: 1.05, backgroundColor: "#00ffff", color: "#000" }}
@@ -42,7 +56,7 @@ const HeroSection = () => {
           <motion.a
             whileHover={{ scale: 1.05, backgroundColor: "#00ffff", color: "#000" }}
             whileTap={{ scale: 0.95 }}
-            href="#"
+            href="https://wa.me/5548991771777"
             className={styles.button}
           >
             Entrar em contato
@@ -52,11 +66,23 @@ const HeroSection = () => {
 
       <motion.div
         className={styles.imageContainer}
+        initial={{ scale: 1, rotate: 0 }}
+        animate={
+          animateImage
+            ? { scale: [1, 1.1, 1], rotate: [0, 2, 0], boxShadow: "0 0 20px #00ffff" }
+            : {}
+        }
         whileHover={{ scale: 1.1, rotate: 2 }}
-        transition={{ type: 'spring', stiffness: 300 }}
+        transition={{
+          duration: animateImage ? 1.5 : 0.3,
+          ease: "easeInOut",
+          type: animateImage ? "tween" : "spring",
+          stiffness: 300,
+        }}
       >
         <img src="/rafa.jpg" alt="Rafael Steffens" className={styles.avatar} />
       </motion.div>
+
     </section>
   );
 };
